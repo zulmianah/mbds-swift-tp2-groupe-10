7443ec79740473febc74881b81c6a70a68e243fe
zulmianah <zulmianah@gmail.com>
2021-03-20 16:01:59 +0100

    Personnalisez les cellules

---
+++ data/Countries.swift
@@ -0,0 +1,22 @@
+//
+//  Countries.swift
+//  TP2_Groupe_10
+//
+//  Created by mbdse on 20/03/2021.
+//  Copyright © 2021 com.mbds.madagascar. All rights reserved.
+//
+
+import Foundation
+let countries = [
+    Country(isoCode: "at", name: "Austria", continent: "Europe"),
+    Country(isoCode: "be", name: "Belgium", continent: "Europe"),
+    Country(isoCode: "de", name: "Germany", continent: "Europe"),
+    Country(isoCode: "el", name: "Greece", continent: "Europe"),
+    Country(isoCode: "fr", name: "France", continent: "Europe"),
+    
+    Country(isoCode: "jp", name: "Japan", continent: "Asie"),
+    Country(isoCode: "mg", name: "Madagascar", continent: "Afrique"),
+    Country(isoCode: "id", name: "Indonesia", continent: "Asie"),
+    Country(isoCode: "es", name: "Espagne", continent: "Europe"),
+    Country(isoCode: "in", name: "Inde", continent: "Asie"),
+]
---
+++ models/Country.swift
@@ -0,0 +1,14 @@
+//
+//  Country.swift
+//  TP2_Groupe_10
+//
+//  Created by mbdse on 20/03/2021.
+//  Copyright © 2021 com.mbds.madagascar. All rights reserved.
+//
+
+import Foundation
+struct Country {
+    var isoCode: String
+    var name: String
+    var continent: String
+}
